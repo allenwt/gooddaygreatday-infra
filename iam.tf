acacca49ada2276ebbf077f726031774474eb02f
Will Allen <will.allen@employbridge.comn>
2022-05-03 13:16:36 -0500

    test

---
+++ iam.tf
@@ -0,0 +1,3 @@
+module "iam_account" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-account"
+  version = ">= 4"
---
+++ versions.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "hard-drive-holdings"
+
+    workspaces {
+      name = "gooddaygreatday-infra"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.12.1"
+    }
+  }
+
+  required_version = ">= 1.0.2"
+}
